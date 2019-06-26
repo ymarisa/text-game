7a1093afed36e66d580c650dec6f2322648fb97d
Marisa Yeung <neurosa@gmail.com>
2019-06-26 13:55:15 -0700

    create game instance fn

--- start.py
+++ start.py
@@ -1,17 +1,19 @@
-class Game:
-    def __init__(self):
-        self.locations = [
-            Room("test", [], [], "foobartest", []),
-            Room("test2", [], ["something"], "2foobartest", [])
-        ]
-        self.items = [
-            "gem"
-        ]
-        self.current_location = self.locations[0]
-        self.bag = []
-        self.prompt = f"You are in {self.current_location}. {self.current_location.get_description()} \
-        {self.current_location.get_next_locations()}"
+NORTH = "North"
+SOUTH = "South"
+EAST = "East"
+WEST = "West"
 
+class Game:
+    PROMPT = """You are in {{CURRENT_LOCATION}}. {{DESCRIPTION}} \
+        \n{{OPTIONS}}"""
+    def __init__(self, locations, items, start_location):
+        self.items = items
+        # name, items, doors, description, who
+        self.locations = locations        
+        self.current_location = start_location    #Room object
+        self.bag = []
+
+    #for debugging
     def print_locations(self):
         print(len(self.locations))
         for l in self.locations:
@@ -33,7 +35,10 @@ class Game:
         return self.current_location == self.locations[1] and self.bag.contains("gem")
 
     def get_prompt(self):
-        return ""
+        current_prompt = self.PROMPT.replace("{{CURRENT_LOCATION}}", self.current_location.get_name())
+        current_prompt = current_prompt.replace("{{DESCRIPTION}}", self.current_location.get_description())
+        current_prompt = current_prompt.replace("{{OPTIONS}}", self.current_location.get_next_locations())
+        return current_prompt
 
 # {"North": Room}
 class Room:
@@ -53,12 +58,16 @@ class Room:
     def get_next_locations(self):
         next_locations = ""
         for door in self.doors:
-            next_locations = next_locations + f"{door}: {self.doors[door].get_name()}"
+            next_locations = next_locations + f"{door}: {self.doors[door].get_name()}\n"
+        return next_locations
+
+    def set_door(self, direction, room):
+        self.doors[direction] = room
 
     def __str__(self):
         return self.name
 
-class Person:
+class NPC:
     def __init__(self, item, lines):
         self.location = "test2"
         self.bag = item
@@ -93,10 +102,41 @@ class Lines:
     def answer_no(self):
         return self.answer_no
 
+def make_Castle():
+    items = [
+            "gem",
+            "candlestick"
+    ]
+
+    locations = {
+            "room0": Room("room0", [], {}, "0foobartest", []),
+            "room1": Room("room1", [], {}, "1foobartest", []),
+            "room2": Room("room2", [items[1]], {}, "2foobartest", []),
+    }
+
+    # NPC: item, lines
+    NPCs = {
+        "Woody": NPC(items[0],
+            lines=Lines(greet="Howdy", answer_yes="Yes indeed", answer_no="I'm afraid not"))
+    }
+
+    #TODO: change the ref to self.locations
+    locations["room0"].set_door(SOUTH, locations["room1"])
+    locations["room1"].set_door(NORTH, locations["room0"])
+
+    locations["room1"].set_door(EAST, locations["room2"])
+    locations["room2"].set_door(WEST, locations["room1"])
+    
+    locations["room2"].set_door(EAST, locations["room0"])
+    locations["room0"].set_door(WEST, locations["room2"])
+
+    g = Game(locations, items, locations["room0"])
+    return g
 
 def main():
-    g = Game()
+    g = make_Castle()
     g.print_locations()
+    print(g.get_prompt())
 
 
 
